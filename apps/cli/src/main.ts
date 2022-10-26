5f2799048121f45d5bb2e228afb78cfe752809fd
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-10-26 02:18:42 -0600

    Add progress bar

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -1,14 +1,15 @@
-import { gql, createClient } from '@urql/core';
-import neo4j from 'neo4j-driver';
+import { gql, createClient, Client } from '@urql/core';
+import neo4j, { Session } from 'neo4j-driver';
 import { config } from 'dotenv';
 import Account, { deleteAll as deleteAllAccounts } from './Account';
 import { deleteAll as deleteAllFills, Fill } from './Fill';
+import { Bar } from 'cli-progress';
 
 config();
 
 const fillsQuery = gql`
     query FillsQuery($limit: Int) {
-        fills(limit: $limit, order_by: {timestamp: desc}) {
+        fills(limit: $limit, order_by: { timestamp: desc }) {
             makerToken
             makerTokenSymbol
             takerToken
@@ -21,6 +22,66 @@ const fillsQuery = gql`
         }
     }
 `;
+
+async function createMeshFetch(session: Session, dataApiClient: Client, limit: number, pageSize: number = 5) {
+    const progressBar = new Bar({
+        // green bar, reset styles after bar element
+        format: ' >> [\u001b[32m{bar}\u001b[0m] {percentage}% | ETA: {eta}s | {value}/{total}',
+        // same chars for bar elements, just separated by colors
+        forceRedraw: true,
+        // change color to yellow between bar complete/incomplete -> incomplete becomes yellow
+        barGlue: '\u001b[33m'
+    });
+
+    // start the progress bar with a total value of 200 and start value of 0
+    progressBar.start(limit, 0);
+
+    let offset = 0;
+    while (offset * pageSize < limit) {
+        const results = await dataApiClient
+            .query<{
+                fills: {
+                    makerToken: string;
+                    makerTokenSymbol: string;
+                    takerToken: string;
+                    takerTokenSymbol: string;
+                    volumeUSD: number;
+                    timestamp: string;
+                    chain: {
+                        reference: string;
+                        __typename: string;
+                    };
+                    __typename: string;
+                }[];
+            }>(fillsQuery, { limit: pageSize })
+            .toPromise();
+        // TODO (rhinodavid): Remove force unwrap
+        const { fills } = results.data!;
+
+        const createdAccounts = new Set<string>();
+
+        // TODO (rhinodavid): stream
+        for (let fill of fills) {
+            const makerToken = new Account(fill.makerToken, parseInt(fill.chain.reference), fill.makerTokenSymbol);
+            const takerToken = new Account(fill.takerToken, parseInt(fill.chain.reference), fill.takerTokenSymbol);
+            if (!createdAccounts.has(makerToken.address)) {
+                createdAccounts.add(makerToken.address);
+                await makerToken.create(session);
+            }
+            if (!createdAccounts.has(takerToken.address)) {
+                createdAccounts.add(takerToken.address);
+                await takerToken.create(session);
+            }
+
+            const fillRelationship = new Fill(makerToken, takerToken, fill.volumeUSD, new Date(fill.timestamp));
+            await fillRelationship.create(session);
+        }
+        offset++;
+
+        progressBar.update(offset * pageSize);
+    }
+    progressBar.stop();
+}
 
 async function main() {
     const dataApiClient = createClient({
@@ -35,52 +96,10 @@ async function main() {
     const session = driver.session({ defaultAccessMode: neo4j.session.WRITE });
     await deleteAllFills(session);
     await deleteAllAccounts(session);
-    await (async function routine(limit: number) {
-        const pageSize = 5;
-        let offset = 0;
-        while (offset * pageSize < limit) {
-            console.log({offset, limit, pageSize});
-            const results = await dataApiClient
-                .query<{
-                    fills: {
-                        makerToken: string;
-                        makerTokenSymbol: string;
-                        takerToken: string;
-                        takerTokenSymbol: string;
-                        volumeUSD: number;
-                        timestamp: string;
-                        chain: {
-                            reference: string;
-                            __typename: string;
-                        };
-                        __typename: string;
-                    }[];
-                }>(fillsQuery, { limit: 100 })
-                .toPromise();
-            // TODO (rhinodavid): Remove force unwrap
-            const { fills } = results.data!;
+    /////////////////////////////////////
+    await createMeshFetch(session, dataApiClient, /* limit */ 100, /* page size */ 10);
+    /////////////////////////////////////
 
-            const createdAccounts = new Set<string>();
-
-            // TODO (rhinodavid): stream
-            for (let fill of fills) {
-                const makerToken = new Account(fill.makerToken, parseInt(fill.chain.reference), fill.makerTokenSymbol);
-                const takerToken = new Account(fill.takerToken, parseInt(fill.chain.reference), fill.takerTokenSymbol);
-                if (!createdAccounts.has(makerToken.address)) {
-                    createdAccounts.add(makerToken.address);
-                    await makerToken.create(session);
-                }
-                if (!createdAccounts.has(takerToken.address)) {
-                    createdAccounts.add(takerToken.address);
-                    await takerToken.create(session);
-                }
-
-                const fillRelationship = new Fill(makerToken, takerToken, fill.volumeUSD, new Date(fill.timestamp));
-                await fillRelationship.create(session);
-            }
-            offset++;
-        }
-    })(/* limit */ 100);
     session.close();
     driver.close();
 }
